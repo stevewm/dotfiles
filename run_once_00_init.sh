b5b9d84e4cec9e6a2679e08fbcd4860291045304
stevewm <34654219+duhio@users.noreply.github.com>
2024-06-21 07:40:04 -0700

    commit WIP

---
+++ run_once_00_init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env fish
+
+# Set the wallpaper
+
+# Install fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
