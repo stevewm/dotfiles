2121ada38032b431cc882a86a27bb9408f8d01e7
stevewm <34654219+stevewm@users.noreply.github.com>
2026-05-10 12:21:26 +0200

    manage more things

--- home/.chezmoiscripts/run_onchange_after_gitignore.sh
+++ home/.chezmoiscripts/run_onchange_after_gitignore.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # gitconfig hash: {{ include .config/git/config | sha256sum }}
-# gitignore hash: {{ include .config/git/ignore | sha256sum }}
 git config --global core.excludesFile ~/.config/git/ignore
